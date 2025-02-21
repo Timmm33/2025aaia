108fee5515a42a80fef8e424e9196cf40f74dba1
Timmm33 <tim950303@gmail.com>
2025-02-21 12:05:09 +0800

    week01-1

--- week01-1.cpp
+++ week01-1.cpp
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> ///使用standard標準的 input/output
 
 int main()
 {
-	int a, b;
-	scanf( "%d%d", &a, &b );
-	printf("%d" , a+b );
+	int a, b; ///整數a,b
+	scanf( "%d%d", &a, &b ); /// 讀入資料 (小心 "%d%d" 後面逗號 ///兩個%d%d & &
+	printf("%d" , a+b ); ///應出資料,把答案印出來
 }
