97f2a6c434a5d17ad4c3f5879c98f6f6dba748db
Timmm33 <tim950303@gmail.com>
2025-05-23 12:08:24 +0800

    week14

---
+++ 14週/week14-1.cpp
@@ -0,0 +1,15 @@
+/// week14-1.cpp
+
+#include <stdio.h>
+int main()
+{
+    printf("請輸入分子a、分母b:");
+    int a, b;
+    scanf("%d%d", &a, &b);
+    int ans;
+    for(int i=1; i<=a; i++){
+        if(a%i==0 && b%i==0) ans = i; /// 可整除
+    }
+    printf("最大公因數是: %d\n", ans);
+    printf("%d / %d 等於 %d / %d\n", a, b, a/ans, b/ans);
+}
